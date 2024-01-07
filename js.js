ba3a358b27053cb9b040c6bde1dcd6bda745fbf9
ManuelGL4 <nade95nanu50@gmail.com>
2024-01-07 18:43:25 +0100

    Getter de categories y metodo de añadir categories

--- js.js
+++ js.js
@@ -206,7 +206,43 @@ class Allergen {
   
  // Objeto RestaurantsManager
  class RestaurantsManager {
- 
+  constructor(systemName) {
+    if (RestaurantsManager.instance) {
+      return RestaurantsManager.instance;
+    }
+
+    this.systemName = systemName;
+    this.categoriesMap = new Map(); // coleccion para almacenar categorias
+    this.menusMap = new Map(); // coleccion para almacenar menús 
+    this.allergensMap = new Map(); // coleccion para almacenar alérgenos
+    this.dishesMap = new Map(); //coleccion  para almacenar platos
+    this.restaurantsMap = new Map(); // coleccion para almacenar restaurantes 
+    RestaurantsManager.instance = this;
+  }//Fin constructor
+
+  //Getter de categories
+  getCategories() {
+    //El .values del mapa devuelve un iterable con los valores del mapa
+    return this.categoriesMap.values();
+  }
+
+  //Metodo addCategory
+  addCategory(newCategory) {
+    if (!newCategory || !(newCategory instanceof Category)) {
+      throw new Error('La categoría debe ser un objeto Category');
+    }
+
+    const categoryName = newCategory.getName();
+    
+    if (this.categoriesMap.has(categoryName)) {
+      throw new Error('La categoría ya existe en la coleccion');
+    }
+
+    this.categoriesMap.set(categoryName, newCategory);
+
+    return this; // Para poder encadenar
+  }
+
 }
   
   const dish1 = new Dish('Pasta Carbonara', 'Plato italiano', ['pasta', 'bacon', 'salsa'], '/images/pasta.jpg');
@@ -217,10 +253,18 @@ class Allergen {
   const menu1 = new Menu('Menu 1', 'El primer menu del restaurante');
   const coordinate1 = new Coordinate(40.7128, -74.0060);
   const restaurant1 = new Restaurant('Restaurante 1', 'El mejor restaurante', coordinate1);
-  
+
+  const restaurantsManager = new RestaurantsManager('Sistema del restaurante');
+  restaurantsManager.addCategory(category1);
+  restaurantsManager.addCategory(new Category('Mexicano', 'Cocina Mexicana'))
+
+  const categoriesIterator = restaurantsManager.getCategories();
+  for (const category of categoriesIterator) {
+  console.log(category.toString());
+  }
+
   console.log(allergen1);
   console.log(dish1.toString());
   console.log(category1.toString());
   console.log(menu1);
   console.log(restaurant1);
-  
